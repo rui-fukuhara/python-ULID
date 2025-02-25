79bfb37a7f83f444331f5fa29550f98e84868605
rui-fukuhara <basket85r@gmail.com>
2025-02-25 18:39:09 +0900

    入力された数値分だけULIDを出力する

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 from ulid import new
 
-# 新しいULIDを生成
-ulid = new()
-print(ulid)
+# ユーザーから生成したいULIDの数を入力として受け取る
+try:
+    count = int(input("生成したいULIDの数を入力してください: "))
+    
+    # 指定された数だけULIDを生成
+    for _ in range(count):
+        ulid = new()
+        print(ulid)
+except ValueError:
+    print("正しい数値を入力してください。")
